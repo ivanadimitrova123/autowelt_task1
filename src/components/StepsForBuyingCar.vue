bb1faae728266ea9bc4054b5c0aed640fc4f1aa9
ivanadimitrova123 <ivanadimitrova23@gmail.com>
2022-07-11 13:20:49 +0200

    added component

---
+++ src/components/StepsForBuyingCar.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <li>
+      <p>{{steps.stepName}}</p>
+      <p>{{steps.description}}</p>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "StepsForBuyingCar",
+  props:{
+    steps: Object
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
